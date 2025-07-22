ef8eb995e17f96e2a93126ba9ab8d31069682d03
Collins Kipruto <collinskipruto036@gmail.com>
2025-07-22 13:45:33 +0300

    submit

--- alx-project-0x03/pages/index.tsx
+++ alx-project-0x03/pages/index.tsx
@@ -1,8 +1,8 @@
-const Index: React.FC = () =>{
+const Home: React.FC = () =>{
   return(
     <div>
       <h1>Welcome to Splash App</h1>
     </div>
   )
 }
-export default Index;
+export default Home;
